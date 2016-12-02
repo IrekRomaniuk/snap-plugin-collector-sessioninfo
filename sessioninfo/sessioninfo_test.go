1e7e2e6cc4ec3a1389ad12019a688d9655bd8bcf
irom77 <irek.romaniuk@gmail.com>
2016-12-02 13:21:15 -0500

    test added

--- sessioninfo/sessioninfo_test.go
+++ sessioninfo/sessioninfo_test.go
@@ -40,7 +40,7 @@ func TestSessioninfoPlugin(t *testing.T) {
 			So(collector, ShouldNotBeNil)
 		})
 		Convey("So Sessioninfo collector should be of Sessioninfo type", func() {
-			So(collector, ShouldHaveSameTypeAs, sessioninfo)
+			So(collector, ShouldHaveSameTypeAs, &SessioninfoCollector{})
 		})
 		Convey("collector.GetConfigPolicy() should return a config policy", func() {
 			configPolicy, _ := collector.GetConfigPolicy()
