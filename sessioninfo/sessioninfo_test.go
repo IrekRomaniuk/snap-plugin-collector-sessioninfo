d078fedd820bf30ee453617c1124de49c5e2db6a
irom77 <irek.romaniuk@gmail.com>
2016-12-02 13:12:55 -0500

    test added

--- sessioninfo/sessioninfo_test.go
+++ sessioninfo/sessioninfo_test.go
@@ -33,5 +33,46 @@ func TestSessioninfoPlugin(t *testing.T) {
 		So(meta.Version, ShouldResemble, pluginVersion)
 		So(meta.Type, ShouldResemble, plugin.CollectorPluginType)
 	})
+	Convey("Create Sessioninfo Collector", t, func() {
+		collector := New()
+		Convey("So Sessioninfo collector should not be nil", func() {
+			So(collector, ShouldNotBeNil)
+		})
+		Convey("So Sessioninfo collector should be of Sessioninfo type", func() {
+			So(collector, ShouldHaveSameTypeAs, &Ping{})
+		})
+		Convey("collector.GetConfigPolicy() should return a config policy", func() {
+			configPolicy, _ := collector.GetConfigPolicy()
+			Convey("So config policy should not be nil", func() {
+				So(configPolicy, ShouldNotBeNil)
+				t.Log(configPolicy)
+			})
+			Convey("So config policy should be a cpolicy.ConfigPolicy", func() {
+				So(configPolicy, ShouldHaveSameTypeAs, &cpolicy.ConfigPolicy{})
+			})
+			Convey("So config policy namespace should be /pan/sessioninfo", func() {
+				conf := configPolicy.Get([]string{"pan", "sessioninfo"})
+				So(conf, ShouldNotBeNil)
+				So(conf.HasRules(), ShouldBeTrue)
+				tables := conf.RulesAsTable()
+				So(len(tables), ShouldEqual, 3)
+				for _, rule := range tables {
+					So(rule.Name, ShouldBeIn, "api", "ip", "cmd")
+					switch rule.Name {
+					case "api":
+						So(rule.Required, ShouldBeTrue)
+						So(rule.Type, ShouldEqual, "string")
+					case "ip":
+						So(rule.Required, ShouldBeFalse)
+						So(rule.Type, ShouldEqual, "string")
+					case "cmd":
+						So(rule.Required, ShouldBeFalse)
+						So(rule.Type, ShouldEqual, "string")
+					}
+				}
+			})
+		})
+	})
+}
 }
 
